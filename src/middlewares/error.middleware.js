508ac0bd3e47b1f0f72e6d051e57158d35682d05
Chamod Sathsara <sathsaragcp@gmail.com>
2025-12-24 14:41:20 +0530

    add error midleware

---
+++ src/middlewares/error.middleware.js
@@ -0,0 +1,30 @@
+const errorHandler = (err, req, res, next) => {
+  let statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+  let message = err.message;
+
+  if (err.name === 'ValidationError') {
+    statusCode = 400;
+    message = Object.values(err.errors)
+      .map((e) => e.message)
+      .join(', ');
+  }
+
+  if (err.code === 11000) {
+    statusCode = 400;
+    const field = Object.keys(err.keyPattern)[0];
+    message = `${field} already exists`;
+  }
+
+  if (err.name === 'CastError') {
+    statusCode = 400;
+    message = 'Invalid ID format';
+  }
+
+  res.status(statusCode).json({
+    success: false,
+    message,
+    stack: process.env.NODE_ENV === 'production' ? null : err.stack
+  });
+};
+
+export default errorHandler;
